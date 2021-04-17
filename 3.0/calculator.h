17f936705d164fcd7dff2697132253819869ec22
Quincy <82714219+wowza1234@users.noreply.github.com>
2021-04-17 10:16:45 -0400

    Create calculator.h

---
+++ 3.0/calculator.h
@@ -0,0 +1,13 @@
+#include <string>
+#pragma once
+
+class calculate
+{
+public:
+	std::string add(double numA, double numB);
+	std::string subtract(double numA, double numB);
+	std::string multiply(double numA, double numB);
+	std::string divide(double numA, double numB);
+	std::string power(double numA, double numB);
+	std::string square(double numA, double numB);
+};
